b3af14adcc629c68e2fa74e3872ce9d69a7648d1
Berthold Roiser <berthold.roiser@gmail.com>
2014-11-01 13:18:29 +0100

    save

--- src/at/jku/cp/rau/search/algorithms/ASTAR.java
+++ src/at/jku/cp/rau/search/algorithms/ASTAR.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import at.jku.cp.rau.game.functions.Function;
@@ -20,11 +21,11 @@ public class ASTAR<T extends Node<T>> implements Search<T> {
     private Function<T> heuristic;
 
     private Set<T> closedset = new HashSet<>();
-    private Set<T> openset = new HashSet<>();
+    private PriorityQueue<T> openset = new PriorityQueue<>(20, new CostComparator());
 
     private HashMap<T, T> routes = new HashMap<T, T>();
     private Map<T, Double> costMap = new HashMap<>();
-    private Map<T, Double> functionMap = new HashMap<>();
+    private Map<T, Double> scoreMap = new HashMap<>();
 
     public ASTAR(Function<T> costs, Function<T> heuristic) {
         this.cost = costs;
@@ -34,7 +35,7 @@ public class ASTAR<T extends Node<T>> implements Search<T> {
     @Override
     public List<T> search(T start, Predicate<T> endPredicate) {
         changeMaps(start, 0);
-        addToOpenset(start, 0);
+        addToOpenset(start);
 
         while (!openset.isEmpty()) {
             System.out.println(openset.toString());
@@ -53,9 +54,7 @@ public class ASTAR<T extends Node<T>> implements Search<T> {
                 if (!openset.contains(neighbor) || costOfNeighbor < costMap.get(neighbor)) {
                     addRoute(current, neighbor);
                     changeMaps(neighbor, costOfNeighbor);
-                    if (!openset.contains(neighbor)) {
-                        addToOpenset(neighbor, costOfNeighbor);
-                    }
+                    addToOpenset(neighbor);
                 }
             }
         }
@@ -70,19 +69,24 @@ public class ASTAR<T extends Node<T>> implements Search<T> {
         return Collections.min(openset, new CostComparator());
     }
 
-    private void addToOpenset(T node, double cost) {
+    private void addToOpenset(T node) {
+        if (openset.contains(node)) {
+            openset.remove(node);
+        }
         openset.add(node);
     }
 
     private void changeMaps(T node, double cost) {
         costMap.put(node, cost);
-        functionMap.put(node, cost + heuristic.value(node));
+        scoreMap.put(node, cost + heuristic.value(node));
     }
 
     private final class CostComparator implements Comparator<T> {
         @Override
         public int compare(T o1, T o2) {
-            return functionMap.get(o1).compareTo(functionMap.get(o2));
+            double scoreO1 = scoreMap.get(o1);
+            double scoreO2 = scoreMap.get(o2);
+            return scoreO1 > scoreO2 ? 1 : scoreO1 < scoreO2 ? -1 : 0;
         }
     }
 
